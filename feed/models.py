1dc2da114b7f00dd0a1882565cacccdcfe8ebd34
army04 <yrlee0412@gmail.com>
2019-05-24 22:39:10 +0900

    change blog directory

--- feed/models.py
+++ feed/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class HashTag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     DEVELOPMENT = "dv"
     PERSONAL = "ps"
@@ -17,19 +23,19 @@ class Article(models.Model):
         default=DEVELOPMENT
     )
 
+    hashtag = models.ManyToManyField(HashTag)
+
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(
+    Article,
+    related_name="article_comments",
+    on_delete=models.CASCADE
+    )
     username = models.CharField(max_length=50)
     content = models.CharField(max_length=200)
 
     def __str__(self):
         return "{}에 댓글 : {}".format(self.article.title, self.content)
-
-class HashTag(models.Model):
-    name = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name
--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,63 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Article, Comment, HashTag
 
 # Create your views here.
 def index(request):
-    pass
+    # GET & POST (Article을 category 분류해서 보여줌)
+    category = request.GET.get("category")
+    hashtag = request.GET.get("hashtag")
 
-def detail(request):
-    pass
+    hashtag_list = HashTag.objects.all()
+    if not category and not hashtag:
+        article_list = Article.objects.all()
+    elif category:
+        article_list = Article.objects.filter(category=category)
+    else:
+        article_list = Article.objects.filter(hashtag__name=hashtag)
+
+    # category_list=set([])
+    # for article in article_list:
+    #     category_list.add(article.get_category_display())
+    # print(category_list)
+    category_list = set([
+        (article.category, article.get_category_display())
+        for article in article_list
+    ])
+
+    ctx = {
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list,
+        "category_list" : category_list
+    }
+    return render(request, "index.html",ctx)
+
+def detail(request,article_id):
+    article = Article.objects.get(id=article_id)
+    # comment_list = Comment.objects.filter(article__id=article_id)
+    # comment_list = article.article_comments.all()
+    hashtag_list = HashTag.objects.all()
+    ctx = {
+        "article" : article,
+        # "comment_list" : comment_list,
+        "hashtag_list" : hashtag_list
+        }
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        content = request.POST.get("content")
+        Comment.objects.create(
+            article = article,
+            username = username,
+            content = content
+        )
+
+        return HttpResponseRedirect("/{}/".format(article_id))
+        # print(username)
+        # print(content)
+    return render(request, "detail.html",ctx)
 
 # def about(request):
 #     pass
